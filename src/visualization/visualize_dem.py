e20b015209c1632b12d76707a6e8d81215a43347
jonwag13 <jon.wag13@gmail.com>
2019-11-25 16:05:52 -0700

    Adding Snotel Data from Timp Divide (820) for 2010-2013.

--- src/visualization/visualize_dem.py
+++ src/visualization/visualize_dem.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import rasterio as rio
 import os
 
 
@@ -15,9 +16,10 @@ input_dir = os.path.abspath(os.path.join(this_dir,'../../data/processed'))
 output_dir = os.path.abspath(os.path.join(this_dir,'../../reports/figures'))
 
 os.chdir(input_dir)
-im = Image.open('12TVK4470.tif')
-imarray = np.array(im)
-print(imarray)
+# im = Image.open('12TVK4470.tif')
+# imarray = np.array(im)
 
-plt.imshow(imarray)
+lidar = rio.open('12TVK4470.tif')
+
+plt.imshow(lidar.read(1),cmap='pink')
 plt.show()
